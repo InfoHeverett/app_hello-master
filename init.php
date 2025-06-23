d6ca2d6fdff6d2031f1bd248234011e7de8a8922
InfoHeverett <informatique@heverett.com>
2025-06-23 14:54:20 +0200

    Update init.php
    Better data

--- init.php
+++ init.php
@@ -1,5 +1,5 @@
 <?php
 
-define('APP_KEY', 'bde6bdbf559d40feec34 ');
-define('APP_REFERENCE', '06536012ef1fe961a358');
+define('APP_KEY', 'bde6bdbf559d40feec34');
+define('APP_REFERENCE', 'APP2966');
 define('APP_CODE', '');
